a96b4da30cf8534b34a9121f0d9e474253558d0d
jschof1 <jackschofield1@googlemail.com>
2022-08-16 10:02:56 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-szk1ml
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-szk1ml)
---
+++ index.js
@@ -0,0 +1,55 @@
+const Ajv = require('ajv').default;
+const AjvFormats = require('ajv-formats');
+const ajv = new Ajv({ useDefaults: true });
+const betterAjvErrors = require('better-ajv-errors').default;
+AjvFormats(ajv);
+
+const obj = {
+  username: 'martyn',
+  techs: {
+    javascript: true,
+    php: true,
+    rust: false,
+  },
+  exp: 1,
+  date: '2020-12-12',
+  email: 'martyn@gmail.com',
+};
+
+const schema = {
+  type: 'object',
+  properties: {
+    username: { type: 'string', minLength: 1 },
+    techs: {
+      type: 'object',
+      properties: {
+        javascript: { type: 'boolean' },
+        php: { type: 'boolean' },
+        rust: { type: 'boolean' },
+      },
+      required: ['javascript', 'php', 'rust'],
+    },
+    exp: { type: 'number' },
+    date: { type: 'string', format: 'date-time', default: '' },
+    email: { type: 'string', format: 'email-format' },
+    year: { type: 'number', default: 1 },
+  },
+  required: ['username', 'techs', 'exp', 'date', 'email', 'year'],
+};
+
+const dateRegex = /([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))/;
+const emailRegex =
+  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+ajv.addFormat('date-time', dateRegex);
+ajv.addFormat('email-format', emailRegex);
+
+const check = async (data, structure) => {
+  const validate = ajv.compile(structure);
+  const resp = validate(data);
+  const output = betterAjvErrors(structure, data, validate.errors);
+  console.log('hi');
+  console.log(output);
+  return output;
+};
+
+check(obj, schema).then(console.log); // true
